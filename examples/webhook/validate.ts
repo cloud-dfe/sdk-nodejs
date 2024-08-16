64783355dfaebe534b3dd93390354ede1373eb7b
SirHowl <grantd.ctt@gmail.com>
2024-08-16 15:16:17 -0300

    Update validate.ts

--- examples/webhook/validate.ts
+++ examples/webhook/validate.ts
@@ -2,14 +2,26 @@ import Webhook from "../../src/Webhook";
 
 export default function validate() {
 
+    // * Este exemplo de verificação da assinatura dos dados enviados via webhook
+    // *
+    // * Este método indica se os dados passados pelo webhook são validodos
+    // *
+    // * NOTA: Será retornado uma Exception nos seguintes casos:
+    // * 1 - o payload não é um JSON válido
+    // * 2 - o payload não contêm a assinatura (campo signature)
+    // * 3 - o token está vazio (não foi passado um token)
+    // * 4 - Token ou assinatua incorreta (não foi possivel decriptar a assinatura)
+    // * 5 - a assinatura expirou (quando a assinatura tiver sido feita a mais de 5 minutos atras)
+
     try {
 
-    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbXAiOjAsInVzciI6IjgiLCJ0cCI6MSwiaWF0IjoxNTg1NjkxNjA5fQ.s4GHUmafp_8Dkl3Hiiqe9KtHa5RLUg3X_2n6RIEXK34';
-    const payload = '{"signature": "ojAm16Ye1cnnSxIM1D/8uUZROFYMitC6YleumaQx5W5IstqC1pdjvlact1q6LbE9f0OEjbtXZeVPYK/PtOfTmw=="}';
+    // token da softhouse do ambiente sendo usado (lembre-se existem dois token um para homologação e outro para produção e são diferentes)
+    const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbXAiOiJ0b2tlbl9leGVtcGxvIiwidXNyIjoidGsiLCJ0cCI6InRrIn0.Tva_viCMCeG3nkRYmi_RcJ6BtSzui60kdzIsuq5X-sQ";
 
-    const isValid = Webhook.isValid(token, payload);
+    // payload do webhook em JSON (https://doc.cloud-dfe.com.br/webhook)
+    const payload = '{"origem": "TESTE", "cnpj_cpf": "12345678000123", "signature": "tBQrTEui9FxaU7AdFbqPaveg3tBPZ1RjKj3Ytn15fm10/AYIztE6ST+YvLuLu6ea8PUrefX4SpxcT1K8LK40fQ=="}';
 
-    console.log(isValid);
+    return Webhook.isValid(token, payload);
 
     } catch (e: any) {
         console.error(e.message);
